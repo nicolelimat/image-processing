1017c61aafd8448ee50fd1a3a27ba539dae9c01d
nicolelimat <nicolelima.eu@gmail.com>
2024-05-20 18:04:16 -0300

    add distance algorithm `distancia` implementation

--- feijoes/contafeijao.c
+++ feijoes/contafeijao.c
@@ -35,6 +35,26 @@ void Union(int parent[], int i, int j){
     parent[y] = x;
 }
 
+/* Código genérico para limiarização desenvolvido em aula (vídeoaula Transformação de Intensidade) 
+    Alterado para retornar uma imagem no formato .PBM para a função @rotula já desenvolvida em aula
+*/
+image limiar(image In, image Out) {
+    int nr = In->nr;
+    int nc = In->nc;
+    int ml = In->ml;
+
+    double T[ml + 1];
+    
+    // Definindo a transformação com limiar fixo de 100
+    for (int i = 0; i < ml + 1; i++)
+        T[i] = i < 100 ? 1 : 0; // Alteração de 0 : ml para 1 : 0
+    
+    // Aplicando a transformação a cada pixel da imagem de entrada
+    for (int i = 0; i < nr * nc; i++)
+        Out->px[i] = (int)T[In->px[i]]; // Atribuindo o valor transformado ao pixel correspondente na imagem de saída
+    return Out;
+}
+
 // Código de rotulação de componentes conexos desenvolvido em aula
 image rotula (image img) {
     image result = img_clone(img);
@@ -82,12 +102,96 @@ image rotula (image img) {
                 }
             }
         }
-    // Operacao pontual percorrendo o out todinho pra preencher com a nova equivalencia
-    for (int i = 0; i < w * h; i++)
-    {
+        // Operacao pontual percorrendo o out todinho pra preencher com a nova equivalencia
+        for (int i = 0; i < w * h; i++){
         out[i] = find(parent, out[i]);
     }
     
+    result->ml = rotulo;
+    return result;
+}
+
+int min2(int a, int b) {
+    return (a < b) ? a : b;
+}
+
+int min3(int a, int b, int c) {
+    return min2(min2(a, b), c);
+}
+
+void distancia(image In) {
+    int nr = In->nr;
+    int nc = In->nc;
+    int *px = In->px;
+    int max = 0; // Maior distância encontrada
+
+    // Calcular a distância esq cima
+    for (int i = 1; i < nr - 1; i++) {
+        for (int j = 1; j < nc - 1; j++) {
+            int p = px[i * nc + j];
+            int r = px[(i - 1) * nc + j];
+            int t = px[i * nc + j - 1];
+            if (p != 0) {
+                // Pixel com a menor distância entre os vizinhos superiores e o pixel atual
+                px[i * nc + j] = min2(r + 1, t + 1);
+            }
+        }
+    }
+
+    // Calcular a distância dir baixo
+    for (int i = nr - 2; i > 0; i--) {
+        for (int j = nc - 2; j > 0; j--) {
+            int p = px[i * nc + j];
+            int r = px[(i + 1) * nc + j];
+            int t = px[i * nc + j + 1];
+            if (p != 0) {
+                px[i * nc + j] = min3(r + 1, t + 1, p);
+                if (max < px[i * nc + j]) {
+                    max = px[i * nc + j];
+                }
+            }
+        }
+    }
+    In->ml = max;
+
+    int threshold = 5;
+
+    // Reescrever a imagem binária
+    for (int i = 0; i < nr; i++) {
+        for (int j = 0; j < nc; j++) {
+            int p = px[i * nc + j];
+            if (p <= threshold) {
+                px[i * nc + j] = 0; // Perto do centro (dentro do raio)
+            } else {
+                px[i * nc + j] = 1; // Fora do raio
+            }
+        }
+    }
+}
+
+image contaFeijao(image img) {
+    ///TODO: transformar a imagem de entrada em binaria (limiarização), separando o fundo das formas
+    image binaria = img_create(img->nr, img->nc, 1, 1);
+    binaria = limiar(img, binaria);
+    // img_put(binaria, "binaria.pbm",BW);
+
+    ///TODO: rotular a imagem binária 
+    image rotulada = rotula(binaria);
+    // img_put(rotulada, "rotulada.pgm",GRAY);
+
+    ///TODO: calcular imagem distância 
+    distancia(rotulada);
+    // img_put(rotulada, "distance.pgm",BW);
+
+    ///TODO: rotular a imagem binária e contar os feijões
+    rotulada = rotula(rotulada);
+    // img_put(rotulada, "rotulada2.pgm",GRAY);
+
+    ///TODO: contar os feijoes
+    int *out = rotulada->px;
+    int w = rotulada->nc;
+    int h = rotulada->nr;
+
     int rotulos_unicos[1000] = {0};
     for (int i = 0; i < w * h; i++) {
         if (out[i] != 0) {
@@ -96,69 +200,21 @@ image rotula (image img) {
     }
 
     int componentes_conexos = 0;
-    for (int i = 0; i < 1000; i++) {
-        if (rotulos_unicos[i] == 1) {
-            componentes_conexos++;
-        }
-    }
+    for (int i = 0; i < 1000; i++){
+        if (rotulos_unicos[i] == 1) componentes_conexos++;
+    }   
 
     printf("#componentes= %.2d\n", componentes_conexos);
-        
-    result->ml = rotulo;
-    // mostrando o parent
-    // for(int i = 0; i < 1000; i++) printf("parent[%d]: %d\n", i, parent[i]);
 
-    return result;
+    return rotulada;
 }
 
-/* Código genérico para limiarização desenvolvido em aula (vídeoaula Transformação de Intensidade) 
-    Alterado para retornar uma imagem no formato .PBM para a função @rotula já desenvolvida em aula
-*/
-image limiar(image In, image Out) {
-    int nr = In->nr;
-    int nc = In->nc;
-    int ml = In->ml;
-
-    double T[ml + 1];
-    
-    // Definindo a transformação com limiar fixo de 95
-    for (int i = 0; i < ml + 1; i++)
-        T[i] = i < 95 ? 1 : 0; // Alteração de 0 : ml para 1 : 0
-    
-    // Aplicando a transformação a cada pixel da imagem de entrada
-    for (int i = 0; i < nr * nc; i++)
-        Out->px[i] = (int)T[In->px[i]]; // Atribuindo o valor transformado ao pixel correspondente na imagem de saída
-    return Out;
-}
-
-void msg(char *s)
-{
+void msg(char *s) {
     printf("\nContagem de feijoes <3 <3 <3");
     printf("\n-------------------------------");
     printf("\nUsage:  %s  image-name[.pgm]\n\n", s);
     printf("    image-name[.pgm] is image file in pgm format \n\n");
     exit(1);
-}
-
-image contaFeijao(image img) {
-    image result;
-    result = img_clone(img);
-
-    // TODO: transformar a imagem de entrada em binaria (limiarização), separando o fundo das formas
-    image binaria = img_create(img->nr, img->nc, 1, 1);
-    binaria = limiar(img, binaria);
-    img_put(binaria, "binaria.pbm",BW);
-
-    // // TODO: rotular a imagem binária 
-    image rotulada = rotula(binaria);
-
-    // TODO: calcular imagem distância 
-
-    // TODO: contar os feijoes
-    // int feijoes = 0;
-    // printf("#componentes= %.2i", feijoes);
-
-    return rotulada;
 }
 
 /*-------------------------------------------------------------------------
@@ -188,8 +244,9 @@ int main(int argc, char *argv[]) {
     img_put(Out, nameOut, GRAY);
 
     //-- show image
-    // sprintf(cmd, "%s %s &", VIEW, nameOut);
+    // sprintf(cmd, "%s %s %s %s %s &", VIEW, nameIn, "binaria.pbm", "distance.pgm","rotulada.pgm");
     // puts(cmd);
+    // system(cmd);
 
     //-- free memory
     img_free(In);
